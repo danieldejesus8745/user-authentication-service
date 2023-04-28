68f41dbb385777e8cf12937e121a35b1bc49e168
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-04-28 00:20:52 -0300

    Change http authenticate method to 'GET'

--- src/main/java/com/userauthentication/controllers/AuthenticationController.java
+++ src/main/java/com/userauthentication/controllers/AuthenticationController.java
@@ -5,7 +5,7 @@ import com.userauthentication.services.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final TokenService tokenService;
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<String> authenticateUser(@RequestHeader(AUTHENTICATION) String credentials) {
         String response = authenticationService.authenticateUser(credentials);
 
